d64ada117a00b84f5f8e024b361d1d041debe2d9
xiaoxin <284070807@qq.com>
2020-12-29 11:00:04 +0800

    day2

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -1,9 +1,12 @@
 package life.majiang.community.controller;
 
 import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,18 +15,28 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private  String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state)
     {
         AccessTokenDTO accesstokenDTO=new AccessTokenDTO();
         //返回html页面说明参数传错了 注意检查参数
-        accesstokenDTO.setClient_id("c1613313d2e5b5169a96");
-        accesstokenDTO.setClient_secret("2544cfbfdcdaea2a25c836d91ec12921d13adbce");
+        accesstokenDTO.setClient_id(clientId);
+        accesstokenDTO.setClient_secret(clientSecret);
         accesstokenDTO.setCode(code);
-        accesstokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accesstokenDTO.setRedirect_uri(redirectUri);
         accesstokenDTO.setState(state);
-        githubProvider.getAccessToken(accesstokenDTO);
+        String accessToken = githubProvider.getAccessToken(accesstokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
--- src/main/java/life/majiang/community/provider/GithubProvider.java
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -1,6 +1,7 @@
 package life.majiang.community.provider;
 import com.alibaba.fastjson.JSON;
 import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
@@ -18,28 +19,29 @@ public class GithubProvider {
                 .build();
         try(Response response=client.newCall(request).execute()){
             String string = response.body().string();
-            System.out.println(string);
-            return string;
+            String token = string.split("&")[0].split("=")[1];
+            System.out.println(token);
+            return token;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
-//    public GithubUser getUser(String accessToken){
-//        OkHttpClient client=new OkHttpClient();
-//        Request request=new Request.Builder()
-//                .url("https://api.github.com/user?access_token=" + accessToken)
-//                .build();
-//        try {
-//            Response response=client.newCall(request).execute();
-//            //把请求接口得到的数组 赋给一个变量
-//            String string = response.body().string();
-//            //把得到的数组 转化成为java类
-//            GithubUser githubUser = JSON.parseObject(string,GithubUser.class);
-//            return githubUser;
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder()
+                .url("https://api.github.com/user")
+                .header("Authorization","token "+ accessToken)
+                .build();
+        try{
+            Response response=client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            System.out.println(githubUser);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
