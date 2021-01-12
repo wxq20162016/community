f994b00bc85223fc2311575a854f53d625de1986
xiaoxin <284070807@qq.com>
2021-01-12 20:50:26 +0800

    add auth app

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -3,6 +3,7 @@ package life.majiang.community.controller;
 import life.majiang.community.dto.PaginationDTO;
 import life.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+    @Value("${github.client.id}")
+    private String clientId;
 
     @Autowired
     private QuestionService questionService;
@@ -25,6 +28,7 @@ public class IndexController {
 
         PaginationDTO pagination=questionService.list(page,size);
             model.addAttribute("pagination",pagination);
+            model.addAttribute("clientId",clientId);
             System.out.println(pagination);
 
         return "index";
