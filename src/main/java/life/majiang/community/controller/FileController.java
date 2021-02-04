accfc69ef5e2c8bc4a21aec805b52417a07745d1
xiaoxin <284070807@qq.com>
2021-02-04 11:26:14 +0800

    finish

--- src/main/java/life/majiang/community/controller/FileController.java
+++ src/main/java/life/majiang/community/controller/FileController.java
@@ -4,10 +4,12 @@ import life.majiang.community.dto.FileDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FileController {
     @RequestMapping("/file/upload")
+    @ResponseBody
     public FileDTO upload(){
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
